7fe5b60c7615e34fb4782ae79fa97b57866cd57e
jotahpeh <horbuch.joao@escola.pr.gov.br>
2025-05-25 22:33:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Crime',
+    'O que é Estelionato?',
+    ' quando o autor usa meios enganosos ou fraudulentos para induzir alguém ao erro, a fim de obter benefícios próprios'
+)
+
+criaCartao(
+    'Roubo',
+    'Qual o maior roubo do Brasil?',
+    'o assalto ao Banco Central em Fortaleza'
+)
+
+criaCartao(
+    'Trafico',
+    'O que é trafico de drogas?',
+    'é um mercado ilegal global dedicado ao cultivo, fabricação, distribuição e venda de substâncias proibidas.'
+)
+
+criaCartao(
+    'lavagem',
+    'lavagem de dinheiro?',
+    'A lavagem de dinheiro consiste em conjunto de operações comerciais ou financeiras que têm como objetivo a incorporação)'
+)
